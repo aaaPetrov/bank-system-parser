28db21db62e78cc4f2a83c4f75adc9ef82eda01c
Artem Petrov <PitPeacePolice@gmail.com>
2021-11-07 21:16:04 +0300

    Code was fixed.

--- src/main/java/com/solvd/banksystemparser/IParse.java
+++ src/main/java/com/solvd/banksystemparser/IParse.java
@@ -1,7 +1,6 @@
 package com.solvd.banksystemparser;
 
 import org.xml.sax.SAXException;
-
 import javax.xml.parsers.ParserConfigurationException;
 import java.io.IOException;
 import java.util.List;
@@ -9,4 +8,5 @@ import java.util.List;
 public interface IParse {
 
     List<Bank> parse(String pathname) throws ParserConfigurationException, IOException, SAXException;
+
 }
--- src/main/java/com/solvd/banksystemparser/MainClass.java
+++ src/main/java/com/solvd/banksystemparser/MainClass.java
@@ -7,7 +7,6 @@ import org.w3c.dom.Document;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
-
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
@@ -149,4 +148,5 @@ public class MainClass implements IParse {
         }
         return banks;
     }
+
 }
